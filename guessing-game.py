e650fe1cb0a002caa28ae95bbe9223ade5c73de9
Lucas Lima <lucaslima2458@gmail.com>
2020-10-05 14:48:46 -0300

    added comments and imported the module cute_print

--- guessing-game.py
+++ guessing-game.py
@@ -1,13 +1,17 @@
 # Stupid yet another guessing game, why not
 import random
+import cute_print
 
 
 lives = 5
-secret = random.randint(0, 30)
+secret = random.randint(0, 15)
 
+cute_print.cute_print()
 
 while True:
     guess = int(input("Guess the number: "))
+
+    # checking if the guess isn't right
     if guess != secret:
         lives -= 1
         if lives == 0:
@@ -15,5 +19,7 @@ while True:
             break
         else:
             print(f"You have {lives} lives left.")
+
+    # if the guess is right, print that to the user
     else:
-        print("yay")
+        print(f"yay, {secret} was the answer")
